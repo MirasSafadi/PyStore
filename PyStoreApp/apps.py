83a953c715712dbfefa48486edd2c82528d4ecd7
MirasSafadi <safadimiras@gmail.com>
2020-03-30 19:01:43 +0300

    Initial commit - project created

---
+++ PyStoreApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PystoreappConfig(AppConfig):
+    name = 'PyStoreApp'
---
+++ PyStoreEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Pc\Desktop\Python Backend\Python project\PyStore\PyStore\PyStoreEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
